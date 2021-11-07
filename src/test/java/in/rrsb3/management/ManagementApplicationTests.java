12d690e0ecd7977e732bcb8921807abd12a7e43c
ashwani2797 <ashwani2797@gmail.com>
2021-11-07 16:51:13 +0530

    Initial commit

---
+++ src/test/java/in/rrsb3/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rrsb3.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
